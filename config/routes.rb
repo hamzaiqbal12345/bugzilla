624e88bf80f656d0741f132088e4ec211671d4f6
Dev <h.iqbal@devsinc.com>
2022-09-13 15:33:23 +0500

    added only for routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :projects do
-        resources :bugs
+      resources :projects, only: [:index, :show] do
+        resources :bugs, only: [:index, :show]
       end
     end
   end
